898bef22fec928562ef325c00586c17490687582
Luka khurtsidze <khurtsidzeluka0@gmail.com>
2025-07-31 23:39:51 +0400

    Encryption added, still with some bugs

--- MID/password_manager.py
+++ MID/password_manager.py
@@ -1,18 +1,43 @@
-master_pwd = input("What is your master password? ")
+from cryptography.fernet import Fernet
+
+def load_key():
+    try:
+        file = open("key.key", "rb")
+        key = file.read()
+        file.close()
+        return key
+    except FileNotFoundError:
+        # If key doesn't exist, generate and save a new one
+        key = Fernet.generate_key()
+        with open("key.key", "wb") as key_file:
+            key_file.write(key)
+        return key
+
+key = load_key()
+fer = Fernet(key)
 
 def view():
-    pass
+    try:
+        with open('passwords.txt', 'r') as f:
+            for line in f.readlines():
+                data = line.rstrip()
+                if "|" not in data:  # Skip malformed lines
+                    continue
+                user, passw = data.split("|")
+                print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
+    except FileNotFoundError:
+        print("No passwords stored yet.")
 
 def add():
     name = input("Account name: ")
     pwd = input("Password: ")
 
-    with open("passwords.txt" ,'a') as f:
-        f.write(name + "|" + pwd)
-
+    with open('passwords.txt', 'a') as f:
+        encrypted_pwd = fer.encrypt(pwd.encode()).decode()
+        f.write(name + '|' + encrypted_pwd + "\n")
 
 while True:
-    mode = input("Would you want to add a new password or view existing ones? (view/add) , press 'q' to quit ")
+    mode = input("Would you like to add a new password or view existing ones? (view/add), press 'q' to quit: ").lower()
     if mode == "q":
         break
 
